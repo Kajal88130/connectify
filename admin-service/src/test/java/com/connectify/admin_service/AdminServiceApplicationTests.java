51ef402434e39f9d751a6551a4ea56e1737b5af1
seli <selisinha22@gmail.com>
2025-07-15 12:00:50 +0530

    Added the user and admin service

---
+++ admin-service/src/test/java/com/connectify/admin_service/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectify.admin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
